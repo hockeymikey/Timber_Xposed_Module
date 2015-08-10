96c8e6cfdcd6db2e717655f857ee109fc86762db
Poul Erik Venø Hansen <fcknghst@gmail.com>
2015-08-11 00:47:18 +0200

    Fully working for 4.2.3

--- app/src/main/java/proofofconcept/dontsuemeplz/timber/Main.java
+++ app/src/main/java/proofofconcept/dontsuemeplz/timber/Main.java
@@ -1,9 +1,13 @@
 package proofofconcept.dontsuemeplz.timber;
 
+import static de.robv.android.xposed.XposedHelpers.findAndHookConstructor;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.newInstance;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 import android.widget.Toast;
 import android.app.Activity;
@@ -15,84 +19,90 @@ import org.json.JSONObject;
 
 public class Main implements IXposedHookLoadPackage {
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-        if (!lpparam.packageName.equals("com.tinder")){
+        if (!lpparam.packageName.equals("com.tinder")) {
             return;
         }
 
+        final Class class1 = XposedHelpers.findClass(Obfuscator.TINDER_MANAGER_CLASS_KEYS, lpparam.classLoader);
 
-        //XposedBridge.log("We are in Tinder!");
+        //XposedHelpers.findClass("com.tinder.adapters.g$b", lpparam.classLoader); //FIXME what does it do?
+
+        XposedBridge.hookAllMethods(class1, "a", new XC_MethodHook() { //FIXME still "a"?
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                for (final Object o : param.args) {
+                    if (o != null) {
+                        XposedBridge.log("curr=" + o.toString() + "- instanceOf=" + o.getClass());
+                    }
+                }
+            }
+        });
 
         findAndHookMethod("com.tinder.activities.ActivityMain", lpparam.classLoader, "onCreate", Bundle.class, new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                Activity profile_activity = (Activity)param.thisObject;
+                Activity profile_activity = (Activity) param.thisObject;
                 Toast.makeText(profile_activity.getApplicationContext(), "Timber for Tinder 4.2.3", Toast.LENGTH_SHORT).show();
             }
         });
 
-        findAndHookMethod("com.tinder.managers.q", lpparam.classLoader, "ak", new XC_MethodHook() {
+        findAndHookMethod(Obfuscator.TINDER_MANAGER_CLASS_KEYS, lpparam.classLoader, Obfuscator.KEY_TINDER_PLUS_ENABLED_BOOLEAN, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                //4.0.5 -> "com.tinder.managers.r", lpparam.classLoader, "ad", new XC_MethodHook()
                 //XposedBridge.log("Tinder KEY_TINDER_PLUS_ENABLED boolean");
                 param.setResult(true);
             }
         });
 
-        findAndHookMethod("com.tinder.managers.q", lpparam.classLoader, "ah", new XC_MethodHook() {
+        findAndHookMethod(Obfuscator.TINDER_MANAGER_CLASS_KEYS, lpparam.classLoader, Obfuscator.KEY_HAS_PLUS_SUBSCRIPTION_ENABLED_BOOLEAN, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                //4.0.5 -> "com.tinder.managers.r", lpparam.classLoader, "ab", new XC_MethodHook()
-
                 //XposedBridge.log("Tinder KEY_HAS_PLUS_SUBSCRIPTION_ENABLED boolean");
                 param.setResult(true);
             }
         });
 
-        findAndHookMethod("com.tinder.managers.r", lpparam.classLoader, "A", new XC_MethodHook() {
+        findAndHookMethod(Obfuscator.TINDER_MODEL_CLASS, lpparam.classLoader, Obfuscator.TINDER_HASHMAP, String.class, Object.class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                //XposedBridge.log("Tinder LONGITUDE double:"+param.getResult());
+                XposedBridge.log("Tinder HashMap " + param.args[0] + " " + param.args[1]);
+                final String s = (String) param.args[0];
+                if (s.equalsIgnoreCase("tinderPlus") || s.equalsIgnoreCase("KEY_TINDER_PLUS_ENABLED") || s.equalsIgnoreCase("KEY_TINDER_PLUS_ENABLED") || s.equalsIgnoreCase("KEY_HAS_PLUS_SUBSCRIPTION_ENABLED")) {
+                    param.args[1] = true;
+                }
+                if (s.equalsIgnoreCase("HAS_VIEWED_ROADBLOCK")) {
+                    param.args[1] = false;
+                }
+                if (s.equalsIgnoreCase("percentLikesLeft")) {
+                    param.args[1] = "100";
+                }
+                if (s.equalsIgnoreCase("timeRemaining")) {
+                    param.args[1] = "0";
+                }
             }
         });
 
-        findAndHookMethod("com.tinder.managers.r", lpparam.classLoader, "z", new XC_MethodHook() {
+        findAndHookMethod(Obfuscator.TINDER_MANAGER_CLASS_NUMBERS, lpparam.classLoader, Obfuscator.GET_LONG, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                //XposedBridge.log("Tinder LATITUDE double:"+param.getResult());
+                XposedBridge.log("get Long =" + param.getResult());
+                param.setResult(0L);
             }
         });
 
-        findAndHookMethod("com.tinder.managers.l", lpparam.classLoader, "d", new XC_MethodHook() {
+        findAndHookMethod(Obfuscator.TINDER_MANAGER_CLASS_NUMBERS, lpparam.classLoader, Obfuscator.GET_PERCENTAGE, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                //XposedBridge.log("userTraveling boolean:"+param.getResult());
-                //param.setResult(true);
+                XposedBridge.log("real % =" + param.getResult());
+                param.setResult(100);
             }
         });
 
-        findAndHookMethod("com.tinder.activities.ActivityMain", lpparam.classLoader, "a", int.class, new XC_MethodHook() {
+        findAndHookMethod(Obfuscator.TINDER_FRAGMENTS_CLASS, lpparam.classLoader, Obfuscator.NUMBER, new XC_MethodHook() {
             @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                //XposedBridge.log("Tinder Swipe Limit Roadblock boolean");
-                param.setResult(true);
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                param.args[0] = 100;
             }
         });
-
-        //findAndHookMethod("com.tinder.parse.e", lpparam.classLoader, "c", org.json.JSONObject.class, new XC_MethodHook() {
-        //    @Override
-        //    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-        //        JSONObject var0 = (JSONObject) param.thisObject;
-        //        if(var0 != null && var0.toString()!= null) {
-        //            XposedBridge.log("Tinder JSON");
-        //            XposedBridge.log(var0.toString());
-        //        }
-        //var0.put("administrative_area_level_2","Value");
-        //var0.put("locality","Value");
-        //var0.put("street_address","Tinder Land ;)");
-        //var0.put("lat",33.811113D);
-        //var0.put("lon",-117.921584D);
-        //    }
-        //});
     }
 }
---
+++ app/src/main/java/proofofconcept/dontsuemeplz/timber/Obfuscator.java
@@ -0,0 +1,23 @@
+package proofofconcept.dontsuemeplz.timber;
+
+public class Obfuscator {
+
+    //classes
+    public static final String TINDER_MANAGER_CLASS_KEYS = "com.tinder.managers.q"; //was r
+    public static final String TINDER_MODEL_CLASS = "com.tinder.model.m"; //was i
+    public static final String TINDER_MANAGER_CLASS_NUMBERS = "com.tinder.managers.n";
+    public static final String TINDER_FRAGMENTS_CLASS = "com.tinder.fragments.f"; // was u
+
+    //methods
+    public static final String KEY_TINDER_PLUS_ENABLED_BOOLEAN = "ak"; //was ad
+    public static final String KEY_HAS_PLUS_SUBSCRIPTION_ENABLED_BOOLEAN = "ah"; //was ab
+    public static final String TINDER_HASHMAP = "a"; //still a but now returns itself? Might be here we fuck up.
+    public static final String GET_LONG = "b"; //not sure what it does //TODO still b?
+    public static final String GET_PERCENTAGE = "a"; //not sure what it does //TODO still a?
+    public static final String NUMBER = "f"; //was b //Not sure what it does
+
+
+
+
+
+}
